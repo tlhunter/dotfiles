3d69371a063d154f7701b2b33bae9971ef384145
Thomas Hunter II <me@thomashunter.name>
2014-09-16 01:03:07 -0700

    X Windows

--- install.sh
+++ install.sh
@@ -1,5 +1,12 @@
 #!/usr/bin/env sh
 
+# Universal
 ln -s ~/.dotfiles/tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/gitconfig ~/.gitconfig
 ln -s ~/.dotfiles/gitignore ~/.gitignore
+
+# Linux Only
+ln -s ~/.dotfiles/Xmodmap ~/.Xmodmap
+ln -s ~/.dotfiles/xinitrc ~/.xinitrc
+
+# OS X Only
