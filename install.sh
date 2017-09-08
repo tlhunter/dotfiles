0a431cf15cd13bcb40b113e78d8f46368c2cdb3f
Thomas Hunter II <thunter@opentable.com>
2017-09-08 11:48:26 -0700

    ~/.config

--- install.sh
+++ install.sh
@@ -8,7 +8,10 @@ ln -s ~/.dotfiles/gitignore ~/.gitignore
 ln -s ~/.dotfiles/zshrc ~/.zshrc
 
 # Linux Only
-#ln -s ~/.dotfiles/Xmodmap ~/.Xmodmap
-#ln -s ~/.dotfiles/xinitrc ~/.xinitrc
+ln -s ~/.dotfiles/Xmodmap ~/.Xmodmap
+ln -s ~/.dotfiles/xinitrc ~/.xinitrc
+ln -s ~/.dotfiles/config/openbox ~/.config/openbox
+ln -s ~/.dotfiles/config/terminator ~/.config/terminator
+ln -s ~/.dotfiles/config/tint2 ~/.config/tint2
 
 # OS X Only
